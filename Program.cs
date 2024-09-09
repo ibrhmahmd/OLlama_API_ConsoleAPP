c6cc94d380e02ee147bb582544c52a767bd042be
ibrhmahmd <aebrahem743@gmail.com>
2024-09-09 11:20:37 +0300

    adding ongoing while loop chat

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using OllamaClient;
-using Microsoft.Extensions.DependencyInjection;
-using OllamaClient;
 using OllamaClient.Extensions;
 
 namespace Ollama_API_Teting
@@ -10,21 +8,25 @@ namespace Ollama_API_Teting
     {
         static async Task Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
             var serviceProvider = new ServiceCollection()
                       .AddOllamaClient()
                       .BuildServiceProvider();
 
             var client = serviceProvider.GetRequiredService<IOllamaHttpClient>();
 
+            const string modelName = "phi3"; // matches one of the installed models
+
+            // Synchronous call to Pull
             var pullResult = client.Pull(new OllamaClient.Models.PullRequest()
             {
-                Name = "phi3",
+                Name = modelName
             }, CancellationToken.None);
+
 
             Console.WriteLine(pullResult.Status);
 
 
+            // Synchronous call to GetModels
             var models = client.GetModels(CancellationToken.None);
 
             //foreach (var model in models.Models)
@@ -32,27 +34,40 @@ namespace Ollama_API_Teting
             //    Console.WriteLine(model.Name);
             //}
 
-            var result = client.SendChat(new OllamaClient.Models.ChatStreamRequest()
+            while (true)
             {
-                Model = "phi3",
+                Console.WriteLine("\nEnter :");
+                var prompt = Console.ReadLine();
 
-                Messages = new List<OllamaClient.Models.Message>()
-                { new OllamaClient.Models.Message()
+                if (string.IsNullOrWhiteSpace(prompt))
+                {
+                    Console.WriteLine("Prompt cannot be empty, please enter a valid prompt.");
+                }
+                else
+                {
+                    var result = client.SendChat(new OllamaClient.Models.ChatStreamRequest()
                     {
-                        Role = "user",
-                        Content = "Hello, how are you?"
+                        Model = "phi3",
+
+                        Messages = new List<OllamaClient.Models.Message>()
+                        {
+                            new OllamaClient.Models.Message()
+                            {
+                                Role = "user",
+                                Content = prompt
+                            }
+                        }
+                    }, CancellationToken.None);
+
+                    await foreach (var message in result)
+                    {
+                        if (!string.IsNullOrEmpty(message?.Message?.Content))
+                        {
+                            Console.Write(modelName + " : " + message.Message.Content);
+                        }
                     }
                 }
-            }, CancellationToken.None);
-
-
-            await foreach (var message in result)
-                  {
-                if (message.Message is null) continue;
-
-                Console.Write(message.Message.Content);
-                  }
-
+            }
         }
     }
 }
