663a4c3d45c177bc743c14ef655de444415d9d1c
ibrhmahmd <aebrahem743@gmail.com>
2024-09-12 21:11:38 +0300

    Layers Outline

---
+++ Olama_MVC_Layer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Olama_MVC_Layer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
